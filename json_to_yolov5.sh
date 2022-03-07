4198238cf3793efbee7458d6a48b72419340223c
labserver <uzbzarif@gmail.com>
2022-03-07 21:29:13 +0900

    output

--- json_to_yolov5.sh
+++ json_to_yolov5.sh
@@ -1,3 +1,5 @@
 #run json to yolov5
-input_dir='/media/user/hdd2/aihubkr_visualization/dataset/labels'
-python json_to_yolov5.py --input_dir $input_dir
+input_dir='/mnt/hdd2/aihubkr_visualization/ConcreteCrack/labels'
+output_dir='/mnt/hdd2/yolov5_train/workspace'
+label='ConcreteCrack'
+python json_to_yolov5.py --input_dir $input_dir --output_dir $output_dir --label $label
